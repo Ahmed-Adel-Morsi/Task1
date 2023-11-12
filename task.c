f0c73cb12a57f63c2d9a7a3f57f364ee6b286371
Ahmed Adel <ahmedadel0239@gmail.com>
2023-11-12 23:59:47 +0200

    Your commit message

--- task.c
+++ task.c
@@ -1,17 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 
-enum ActivationStatus
-{
-    FALSE = 0,
-    TRUE = 1
-};
-
 struct User
 {
     char username[50];
     char password[50];
-    enum ActivationStatus isActive;
 };
 
 void registerUser(struct User users[], int *userCount)
@@ -20,25 +13,6 @@ void registerUser(struct User users[], int *userCount)
     scanf("%s", users[*userCount].username);
     printf("Enter password: ");
     scanf("%s", users[*userCount].password);
-
-    int activationInput;
-    int check = 1;
-    do
-    {
-        printf("Enter Activation (1 for active, 0 for inactive): ");
-        scanf("%d", &activationInput);
-
-        if (activationInput == TRUE || activationInput == FALSE)
-        {
-            check = 0;
-        }
-        else
-        {
-            printf("Invalid input for activation status. Please enter 1 for active or 0 for inactive.\n");
-        }
-    } while (check);
-
-    users[*userCount].isActive = activationInput;
     (*userCount)++;
     printf("Registration successful!\n");
 }
@@ -47,7 +21,7 @@ int loginUser(struct User users[], int userCount, char username[], char password
 {
     for (int i = 0; i < userCount; i++)
     {
-        if (strcmp(users[i].username, username) == 0 && strcmp(users[i].password, password) == 0 && users[i].isActive == TRUE)
+        if (strcmp(users[i].username, username) == 0 && strcmp(users[i].password, password) == 0)
         {
             return 1;
         }
@@ -88,7 +62,7 @@ int main()
             }
             else
             {
-                printf("Login failed. Invalid username or password or user is inactive.\n");
+                printf("Login failed. Invalid username or password.\n");
             }
             break;
         case 3:
